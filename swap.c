16091a6048f1f8a2065bdbf7dfffd8a31fb8b27e
Leonard-Nfor <leonardnforkort@gmail.com>
2023-08-24 10:43:54 -0700

    swap me

--- swap.c
+++ swap.c
@@ -1,31 +1,31 @@
 #include "monty.h"
 /**
- * f_swap - adds the top two elements of the stack.
+ * f_swap - swapa the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void swap(stack_t **head, unsigned int counter)
+void swap(stack_t **head, unsigned int num)
 {
-	stack_t *h;
-	int len = 0, aux;
+	stack_t *temp;
+	int len = 0, m;
 
-	h = *head;
-	while (h)
+	temp = *head;
+	while (temp != NULL)
 	{
-		h = h->next;
+		temp = temp->next;
 		len++;
 	}
 	if (len < 2)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", num);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	aux = h->n;
-	h->n = h->next->n;
-	h->next->n = aux;
+	temp = *head;
+	m = temp->n;
+	temp->n = temp->next->n;
+	temp->next->n = m;
 }
