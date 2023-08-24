b134a85e893c22e5fbef0799c59f3515397336e9
Leonard-Nfor <leonardnforkort@gmail.com>
2023-08-24 11:21:08 -0700

    geting better

--- add.c
+++ add.c
@@ -1,32 +1,36 @@
 #include "monty.h"
 /**
- * f_add - adds the top two elements of the stack.
+ * add - adds the top two elements of the stack.
  * @head: stack head
- * @counter: line_number
+ * @num: line_number
  * Return: no return
 */
-void add(stack_t **head, unsigned int counter)
+void add(stack_t **head, unsigned int num)
 {
 	stack_t *h;
-	int len = 0, aux;
+	int len = 0, m;
 
-	h = *head;
-	while (h)
+	temp = *head;
+	while (temp != NULL)
 	{
-		h = h->next;
+		temp = temp->next;
 		len++;
 	}
 	if (len < 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fprintf(stderr, "L%d: can't add, stack too short\n", temp);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	aux = h->n + h->next->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+	temp = *head;
+	/*doing the addition*/
+	m = temp->n + temp->next->n;
+	/*transfering result in the next node*/
+	temp->next->n = m;
+	/*making head to point to the next node*/
+	*head = temp->next;
+	/*deleting the first node*/
+	free(temp);
 }
